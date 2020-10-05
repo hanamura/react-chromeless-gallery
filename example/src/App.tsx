2892bf3f19ab9df36f65a395280cef14d7cb9a3c
Taro Hanamura <m@hanamurataro.com>
2020-10-05 12:07:22 +0900

    Update examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,48 +1,116 @@
 import React from 'react'
-import { Gallery } from 'react-chromeless-gallery'
+import {
+  Gallery,
+  GalleryProvider,
+  GalleryConsumer
+} from 'react-chromeless-gallery'
 import 'react-chromeless-gallery/dist/index.css'
 
 import styles from './App.module.css'
 
 const imageUrls = [
   'https://source.unsplash.com/F-gfrzSIPZo/1600x900',
-  'https://source.unsplash.com/f68m3uHNT_A/1600x1600',
-  'https://source.unsplash.com/xsIOCYmlI1g/900x1600',
-  'https://source.unsplash.com/KMn4VEeEPR8/1600x1200',
-  'https://source.unsplash.com/6ArTTluciuA/1200x1600'
+  'https://source.unsplash.com/f68m3uHNT_A/1600x900',
+  'https://source.unsplash.com/xsIOCYmlI1g/1600x900',
+  'https://source.unsplash.com/KMn4VEeEPR8/1600x900',
+  'https://source.unsplash.com/6ArTTluciuA/1600x900'
 ]
 
-const App = () => (
-  <div className={styles.App}>
-    <h2>1</h2>
-    <Gallery>
-      {imageUrls.map((url) => (
-        <img
-          key={url}
-          className={styles.App_ImageItem}
-          src={url}
-          alt=''
-          style={{ display: 'block', width: '100%' }}
-          draggable={false}
-        />
-      ))}
-    </Gallery>
-    <h2>2</h2>
-    <Gallery>
-      {imageUrls.map((url) => (
-        <a
-          key={url}
-          className={styles.App_AnchorItem}
-          href={url}
-          target='_blank'
-          rel='noopener noreferrer'
-          draggable={false}
-        >
-          <img src={url} alt='' draggable={false} />
-        </a>
-      ))}
-    </Gallery>
-  </div>
-)
+const imageUrlsVariousAspectRatio = [
+  'https://source.unsplash.com/F-gfrzSIPZo/1600x900',
+  'https://source.unsplash.com/f68m3uHNT_A/1600x1200',
+  'https://source.unsplash.com/xsIOCYmlI1g/1600x1600',
+  'https://source.unsplash.com/KMn4VEeEPR8/1600x700',
+  'https://source.unsplash.com/6ArTTluciuA/1600x800'
+]
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <h2>Minimal</h2>
+      <Gallery>
+        {imageUrls.map((url) => (
+          <img
+            key={url}
+            className={styles.App_ImageItem}
+            src={url}
+            alt=''
+            draggable={false}
+          />
+        ))}
+      </Gallery>
+
+      <h2>With links</h2>
+      <Gallery>
+        {imageUrls.map((url) => (
+          <a
+            key={url}
+            className={styles.App_AnchorItem}
+            href={url}
+            target='_blank'
+            rel='noopener noreferrer'
+            draggable={false}
+          >
+            <img src={url} alt='' draggable={false} />
+          </a>
+        ))}
+      </Gallery>
+
+      <h2>Various aspect ratio</h2>
+      <Gallery>
+        {imageUrlsVariousAspectRatio.map((url) => (
+          <img
+            key={url}
+            className={styles.App_ImageItem}
+            src={url}
+            alt=''
+            draggable={false}
+          />
+        ))}
+      </Gallery>
+
+      <h2>With controls</h2>
+      <GalleryProvider>
+        <Gallery>
+          {imageUrls.map((url) => (
+            <img
+              key={url}
+              className={styles.App_ImageItem}
+              src={url}
+              alt=''
+              draggable={false}
+            />
+          ))}
+        </Gallery>
+        <GalleryConsumer>
+          {({ index, length, to, prev, next, hasPrev, hasNext }) => (
+            <div className={styles.App_Control}>
+              <button
+                className={styles.App_ControlPrev}
+                onClick={prev}
+                disabled={!hasPrev}
+              />
+              <ul>
+                {new Array(length).fill(0).map((_, i) => (
+                  <li key={i.toString()}>
+                    <button
+                      className={i === index ? styles.isActive : ''}
+                      onClick={() => to(i)}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <button
+                className={styles.App_ControlNext}
+                onClick={next}
+                disabled={!hasNext}
+              />
+            </div>
+          )}
+        </GalleryConsumer>
+      </GalleryProvider>
+    </div>
+  )
+}
 
 export default App
